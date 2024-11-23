2707308a5640e2e58c73716d1c84d7f443e5e283
Mohammed Waleed <mwaleedx1@gmail.com>
2024-11-23 10:33:14 +0200

    Finishing Employee Model

---
+++ EmployeesApp/Models/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeesApp.Models
+{
+    [Table("Emplyee",Schema = "dbo")]
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Employee ID")]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(150)")]
+        [MaxLength(100)]
+        [Display(Name = "Employee Name")]
+        public string EmployeeName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        [MaxLength(5)]
+        [Display(Name = "Employee Number")]
+        public string EmployeeNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Birth Date")]
+        public DateOnly BirthDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Hiring Date")]
+        public DateOnly HiringDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(12,2)")]
+        [Display(Name = "Gross Salary")]
+        public decimal GrossSalary { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(12,2)")]
+        [Display(Name = "Net Salary")]
+        public decimal NetSalary { get; set; }
+
+        [ForeignKey("Department")]
+        [Required]
+        public int DepartmentId { get; set; }
+
+        [Display(Name = "Department")]
+        [NotMapped]
+        public string DepartmentName { get; set; }
+
+        public virtual Department Department { get; set; }
+
+    }
+}
