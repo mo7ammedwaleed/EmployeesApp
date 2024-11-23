057cc502f2c93284d6fe84df2a9c761e41d81e1f
Mohammed Waleed <mwaleedx1@gmail.com>
2024-11-23 11:31:54 +0200

    Adding ApplicationDbContext

---
+++ EmployeesApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeesApp.Models;
+
+namespace EmployeesApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"server=MOHAMMED-WALEED\SQLEXPRESS;Database=EmployeesApp;Integrated Security=True;Trust Server Certificate=True;");
+        }
+    }
+}
