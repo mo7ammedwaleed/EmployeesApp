b17bcc3e0b8d9fd316f9a52301264b38c8fe4b53
Mohammed Waleed <mwaleedx1@gmail.com>
2024-11-24 11:52:21 +0200

    Finshing Employee View And Finshing Create And Update Operations

--- EmployeesApp/Controllers/EmployeeController.cs
+++ EmployeesApp/Controllers/EmployeeController.cs
@@ -12,5 +12,50 @@ namespace EmployeesApp.Controllers
             List<Employee> employees = _context.Employees.ToList();
             return View(employees);
         }
+
+        public IActionResult Create()
+        {
+            ViewBag.Departments = this._context.Departments.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee model)
+        {
+            ModelState.Remove("EmployeeId");
+            ModelState.Remove("DepartmentName");
+            ModelState.Remove("Department");
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Departments = this._context.Departments.ToList();
+            return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            Employee emp = this._context.Employees.Where(e => e.EmployeeId == id).FirstOrDefault();
+            ViewBag.Departments = this._context.Departments.ToList();
+            return View("Create",emp);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee model)
+        {
+            ModelState.Remove("EmployeeId");
+            ModelState.Remove("DepartmentName");
+            ModelState.Remove("Department");
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Departments = this._context.Departments.ToList();
+            return View("Create",model);
+        }
+
     }
 }
